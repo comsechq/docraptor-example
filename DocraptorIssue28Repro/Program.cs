e85707030bf7a4aeb133c81672c0af2b3245ec99
bounav <benjamin.fourmond@comsechq.com>
2022-10-10 10:25:00 +0100

    Repro

---
+++ DocraptorIssue28Repro/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Web;
+using DocRaptor.Api;
+using DocRaptor.Model;
+
+namespace DocraptorIssue28Repro
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var html = @"<html>
+<body>
+    <p>Hello World</p>
+</body>
+</html>";
+
+            var apiKey = "Not Relevant";
+
+            var docRaptorConfig = new DocRaptor.Client.Configuration {Username = apiKey};
+            var docApi = new DocApi(docRaptorConfig);
+
+            html = HttpUtility.HtmlDecode(html);
+            
+            var isTest = true;
+
+            var doc = new Doc(
+                test: isTest,
+                documentContent: html,
+                name: "file.pdf",
+                documentType: Doc.DocumentTypeEnum.Pdf,
+                princeOptions: new PrinceOptions { Media = "screen" }
+            );
+
+            var pdfBytes = docApi.CreateDoc(doc);
+
+            File.WriteAllBytes("./output.pdf", pdfBytes);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+## Description
+
+This is a simple CLI app used to reproduced the [issue 28](https://github.com/DocRaptor/docraptor-csharp/issues/28).
+
+Why is docraptor compliled with a reference to restsharp that doesn't specifiy a publicKeyToken?
+
+This means if your code has two dependencies on different nuget packages that require a different version of docraptor, the code will fail.
+
+## Steps to reproduce
+
+1. Open the solution with visual studio (tested in VS2022)
+1. Hit F5 to start debugging
+1. A `TypeInitializationException` is thrown.
+
